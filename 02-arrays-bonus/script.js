1c2d4324ccc5814e3d4cf162f47cf336fa05da09
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-08 17:43:18 +0200

    remake 1 exercise

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -2,19 +2,29 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
+
+/* SOLUZIONE CON REVERSE
 const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
 for (let index = 0; index < teachers.length; index++) {
   const element = teachers[index];
   console.log(element);
+}*/
+
+const reversedTeachers = [];
+
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reversedTeachers.push(teachers[i]);
 }
+
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = null;
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(5, 1);
+teachers.splice(1, 1);
 console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
